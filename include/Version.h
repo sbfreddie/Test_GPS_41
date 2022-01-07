dcb4070b072f6cffb4c70f8ec2f38531fcdc174b
sbfreddie <freddie65@comcast.net>
2022-01-07 09:43:15 +0100

    Added commented out options to platformio file.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "1.0.20"
+        #define VERSION "1.0.21"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-01-06 11:46:10.274568"
+        #define BUILD_TIMESTAMP "2022-01-07 09:42:00.240826"
     #endif
     
